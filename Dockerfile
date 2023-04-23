9942f206c6106fc978f382241b1c0ff787355096
Emyr298 <emi.ru298@gmail.com>
2023-04-23 18:52:23 +0700

    deployment: yaml and dockerfile

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.10.8-alpine
+
+ARG DJANGO_KEY
+ARG DATABASE_URL
+ARG ENVIRONMENT
+
+ENV DJANGO_KEY ${DJANGO_KEY}
+ENV DATABASE_URL ${DATABASE_URL}
+ENV ENVIRONMENT ${ENVIRONMENT}
+
+WORKDIR /app
+COPY requirements.txt /app
+RUN pip install -r requirements.txt --no-cache-dir
+COPY . /app
+ENTRYPOINT ["python3"]
+
+EXPOSE 8000
+CMD ["manage.py", "migrate"]
+CMD ["manage.py", "runserver", "0.0.0.0:8000"]
--- personal_website/settings.py
+++ personal_website/settings.py
@@ -19,7 +19,7 @@ load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-IS_PRODUCTION = (os.environ.get('ENVIRONMENT') == 'production')
+IS_PRODUCTION = (os.environ.get('ENVIRONMENT').lower() == 'production')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
